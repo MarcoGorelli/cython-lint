92d867756081fb9f47f91d255ad413c58e212370
Marco Gorelli <marcogorelli@protonmail.com>
2022-08-28 11:25:59 +0100

    use Token

--- cython_lint.py
+++ cython_lint.py
@@ -2,43 +2,50 @@ from __future__ import annotations
 
 import argparse
 import sys
+from typing import Iterator
+from typing import NamedTuple
+from typing import Sequence
 
 from Cython.Compiler.Errors import CompileError
-from Cython.Compiler.ExprNodes import (
-    ImportNode,
-    NameNode,
-    TupleNode,
-    TypecastNode,
-    GeneratorExpressionNode,
-    LambdaNode,
-)
-from Cython.Compiler.Nodes import (
-    CArgDeclNode,
-    CFuncDeclaratorNode,
-    CClassDefNode,
-    CFuncDefNode,
-    CImportStatNode,
-    CNameDeclaratorNode,
-    CPtrDeclaratorNode,
-    CSimpleBaseTypeNode,
-    FusedTypeNode,
-    ForInStatNode,
-    FromCImportStatNode,
-    FromImportStatNode,
-    RaiseStatNode,
-    SingleAssignmentNode,
-)
+from Cython.Compiler.ExprNodes import GeneratorExpressionNode
+from Cython.Compiler.ExprNodes import ImportNode
+from Cython.Compiler.ExprNodes import NameNode
+from Cython.Compiler.ExprNodes import TypecastNode
+from Cython.Compiler.ModuleNode import ModuleNode
+from Cython.Compiler.Nodes import CArgDeclNode
+from Cython.Compiler.Nodes import CClassDefNode
+from Cython.Compiler.Nodes import CFuncDeclaratorNode
+from Cython.Compiler.Nodes import CFuncDefNode
+from Cython.Compiler.Nodes import CImportStatNode
+from Cython.Compiler.Nodes import CNameDeclaratorNode
+from Cython.Compiler.Nodes import CPtrDeclaratorNode
+from Cython.Compiler.Nodes import CSimpleBaseTypeNode
+from Cython.Compiler.Nodes import ForInStatNode
+from Cython.Compiler.Nodes import FromCImportStatNode
+from Cython.Compiler.Nodes import FromImportStatNode
+from Cython.Compiler.Nodes import FusedTypeNode
+from Cython.Compiler.Nodes import Node
+from Cython.Compiler.Nodes import SingleAssignmentNode
 from Cython.Compiler.TreeFragment import parse_from_strings
+from tokenize_rt import src_to_tokens
+from tokenize_rt import tokens_to_src
+
+
+class Token(NamedTuple):
+    name: str
+    lineno: int
+    colno: int
 
 
 class CythonLintError(Exception):
     pass
 
 
-from tokenize_rt import Token, src_to_tokens, tokens_to_src
-
-
-def visit_funcdef(node, filename, lines):
+def visit_funcdef(
+    node: CFuncDefNode,
+    filename: str,
+    lines: Sequence[str],
+) -> int:
     ret = 0
 
     children = list(traverse(node))[1:]
@@ -58,24 +65,20 @@ def visit_funcdef(node, filename, lines):
         for i in children
         if isinstance(i, SingleAssignmentNode) and isinstance(i.lhs, NameNode)
     ]
-    for_loop_vars = []
-    # for i in children:
-    #     if isinstance(i, ForInStatNode):
-    #         for _node in _name_from_tuple(i.target):
-    #             for_loop_vars.append((_node.name, *_node.pos[1:]))
-    defs = [*defs, *simple_assignments, *for_loop_vars]
+    defs = [*defs, *simple_assignments]
 
     args = []
     for i in children:
         if isinstance(i, CArgDeclNode):
-            args += _args_from_cargdecl(i)
+            for _arg in _args_from_cargdecl(i):
+                args.append(_arg)
     if isinstance(node.declarator.base, CNameDeclaratorNode):
         func_name = node.declarator.base.name
     elif isinstance(node.declarator.base, CFuncDeclaratorNode):
         if isinstance(node.declarator.base.base, CNameDeclaratorNode):
             func_name = node.declarator.base.base.name
         else:  # pragma: no cover
-            raise CythonLintError("Unexpected error")
+            raise CythonLintError('Unexpected error')
     else:  # pragma: no cover
         raise CythonLintError()
 
@@ -84,55 +87,68 @@ def visit_funcdef(node, filename, lines):
             _def[0] not in [i[0] for i in names]
             and _def[0] != func_name
             and _def[0] not in [i[0] for i in args]
-        ) and "# no-lint" not in lines[_def[1] - 1]:
-            print(f"{filename}:{_def[1]}:{_def[2]}: '{_def[0]}' defined but unused")
+        ) and '# no-lint' not in lines[_def[1] - 1]:
+            print(
+                f'{filename}:{_def[1]}:{_def[2]}: '
+                f"'{_def[0]}' defined but unused",
+            )
             ret = 1
     return ret
 
 
-def _name_from_cptrdeclarator(node):
+def _name_from_cptrdeclarator(
+    node: CPtrDeclaratorNode | CNameDeclaratorNode,
+) -> CNameDeclaratorNode:
     while isinstance(node, CPtrDeclaratorNode):
         node = node.base
     if isinstance(node, CNameDeclaratorNode):
         return node
-    raise CythonLintError("")  # pragma: no cover
+    raise CythonLintError('')  # pragma: no cover
 
 
-def _args_from_cargdecl(i):
-    args = []
-    if isinstance(i.declarator, CNameDeclaratorNode):
-        if i.declarator.name:
-            args.append((i.declarator.name, *i.declarator.pos[1:]))
-        elif isinstance(i.base_type, (CNameDeclaratorNode, CSimpleBaseTypeNode)):
-            args.append((i.base_type.name, *i.base_type.pos[1:]))
+def _args_from_cargdecl(node: CArgDeclNode) -> Iterator[Token]:
+    if isinstance(node.declarator, CNameDeclaratorNode):
+        if node.declarator.name:
+            yield Token(node.declarator.name, *node.declarator.pos[1:])
+        elif isinstance(
+            node.base_type,
+            (CNameDeclaratorNode, CSimpleBaseTypeNode),
+        ):
+            yield Token(node.base_type.name, *node.base_type.pos[1:])
         else:  # pragma: no cover
             raise CythonLintError(
-                f"Unexpected error, please report bug. Expected CNameDeclaratorNode or CSimpleBaseTypeNode, got {i.base_type}"
+                'Unexpected error, please report bug. '
+                'Expected CNameDeclaratorNode or CSimpleBaseTypeNode, '
+                f'got {node.base_type}',
             )
-    elif isinstance(i.declarator, CPtrDeclaratorNode):
-        args.append((i.declarator.base.name, *i.declarator.base.pos[1:]))
-    elif isinstance(i.declarator, CFuncDeclaratorNode):
-        _args = [_args_from_cargdecl(_arg) for _arg in i.declarator.args]
-        args += _args
-        _base = _name_from_cptrdeclarator(i.declarator.base)
-        args.append((_base.name, *_base.pos[1:]))
+    elif isinstance(node.declarator, CPtrDeclaratorNode):
+        yield (
+            Token(
+                node.declarator.base.name,
+                *node.declarator.base.pos[1:],
+            )
+        )
+    elif isinstance(node.declarator, CFuncDeclaratorNode):
+        for _arg in node.declarator.args:
+            yield from _args_from_cargdecl(_arg)
+        _base = _name_from_cptrdeclarator(node.declarator.base)
+        yield Token(_base.name, *_base.pos[1:])
 
     else:  # pragma: no cover
         raise CythonLintError(
-            f"Unexpected error, please report bug. "
-            f"Expected CPtrDeclaratorNode, got {i.declarator}\n"
-            f"{i.declarator.pos}\n"
+            f'Unexpected error, please report bug. '
+            f'Expected CPtrDeclaratorNode, got {node.declarator}\n'
+            f'{node.declarator.pos}\n',
         )
-    return args
 
 
-def main(code, filename):
+def main(code: str, filename: str) -> int:
     ret = 0
     tokens = src_to_tokens(code)
     exclude_lines = {
         token.line
         for token in tokens
-        if token.name == "NAME" and token.src == "include"
+        if token.name == 'NAME' and token.src == 'include'
     }
 
     code = tokens_to_src(tokens)
@@ -145,35 +161,38 @@ def main(code, filename):
 
     _dir = os.path.dirname(filename)
     included_files = [
-        os.path.join(_dir, line.split()[-1].strip("'\"") + ".in")
+        os.path.join(_dir, line.split()[-1].strip("'\"") + '.in')
         for i, line in enumerate(code.splitlines(keepends=True), start=1)
         if i in exclude_lines
     ]
-    included_text = ""
+    included_text = ''
     for _file in included_files:
-        with open(_file, encoding="utf-8") as fd:
+        with open(_file, encoding='utf-8') as fd:
             content = fd.read()
         included_text += content
-    code = "".join(lines)
+    code = ''.join(lines)
 
     tree = parse_from_strings(filename, code)
     nodes = list(traverse(tree))
-    imported_names = []
+    imported_names: list[Token] = []
     for node in nodes:
         if isinstance(node, FromCImportStatNode):
             imported_names.extend(
-                (imp[2] or imp[1], *imp[0][1:]) for imp in node.imported_names
+                Token(imp[2] or imp[1], *imp[0][1:])
+                for imp in node.imported_names
             )
 
         elif isinstance(node, CImportStatNode):
-            imported_names.append((node.as_name or node.module_name, *node.pos[1:]))
+            imported_names.append(
+                Token(node.as_name or node.module_name, *node.pos[1:]),
+            )
         elif isinstance(node, SingleAssignmentNode) and isinstance(
-            node.rhs, ImportNode
+            node.rhs, ImportNode,
         ):
-            imported_names.append((node.lhs.name, *node.lhs.pos[1:]))
+            imported_names.append(Token(node.lhs.name, *node.lhs.pos[1:]))
         elif isinstance(node, FromImportStatNode):
             for imp in node.items:
-                imported_names.append((imp[1].name, *imp[1].pos[1:]))
+                imported_names.append(Token(imp[1].name, *imp[1].pos[1:]))
     imported_names = sorted(imported_names, key=lambda x: (x[1], x[2]))
 
     for node in nodes:
@@ -189,17 +208,18 @@ def main(code, filename):
         if (
             _import[0] not in [i[0] for i in names]
             and _import[0] not in included_text
-            and "# no-cython-lint" not in lines[_import[1] - 1]
+            and '# no-cython-lint' not in lines[_import[1] - 1]
         ):
             print(
-                f"{filename}:{_import[1]}:{_import[2]+1}: Name {_import[0]} imported but unused"
+                f'{filename}:{_import[1]}:{_import[2]+1}: '
+                f'\'{_import[0]}\' imported but unused',
             )
             ret = 1
 
     return ret
 
 
-def traverse(tree):
+def traverse(tree: ModuleNode) -> Node:
     nodes = [tree]
 
     while nodes:
@@ -212,20 +232,20 @@ def traverse(tree):
         child_attrs = copy.deepcopy(node.child_attrs)
 
         if isinstance(node, CClassDefNode):
-            child_attrs.extend(["bases", "decorators"])
+            child_attrs.extend(['bases', 'decorators'])
         elif isinstance(node, TypecastNode):
-            child_attrs.append("base_type")
+            child_attrs.append('base_type')
         elif isinstance(node, GeneratorExpressionNode):
-            if hasattr(node, "loop"):
-                child_attrs.append("loop")
+            if hasattr(node, 'loop'):
+                child_attrs.append('loop')
             else:  # pragma: no cover
                 raise CythonLintError()
         elif isinstance(node, CFuncDefNode):
-            child_attrs.append("decorators")
+            child_attrs.append('decorators')
         elif isinstance(node, FusedTypeNode):
-            child_attrs.append("types")
+            child_attrs.append('types')
         elif isinstance(node, ForInStatNode):
-            child_attrs.append("target")
+            child_attrs.append('target')
 
         for attr in child_attrs:
             child = getattr(node, attr)
@@ -236,13 +256,13 @@ def traverse(tree):
         yield node
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("paths", nargs="*")
+    parser.add_argument('paths', nargs='*')
     args = parser.parse_args()
     ret = 0
     for path in args.paths:
-        with open(path, encoding="utf-8") as fd:
+        with open(path, encoding='utf-8') as fd:
             content = fd.read()
         try:
             ret |= main(content, path)
--- tests/main_test.py
+++ tests/main_test.py
@@ -1,9 +1,12 @@
+import os
+from typing import Any
+
 import pytest
 
 from cython_lint import main
-import os
 
 INCLUDE_FILE = os.path.join('tests', 'data', 'foo.pxi')
+
 
 @pytest.mark.parametrize(
     'src, expected',
@@ -11,79 +14,81 @@ INCLUDE_FILE = os.path.join('tests', 'data', 'foo.pxi')
         (
             'cdef bint foo():\n'
             '    cdef int a\n',
-            't.py:2:13: \'a\' defined but unused\n'
+            't.py:2:13: \'a\' defined but unused\n',
         ),
-    ]
+    ],
 )
-def test_assigned_unused(capsys, src, expected):
+def test_assigned_unused(capsys: Any, src: str, expected: str) -> None:
     ret = main(src, 't.py')
     out, _ = capsys.readouterr()
     assert out == expected
     assert ret == 1
+
 
 @pytest.mark.parametrize(
     'src, expected',
     [
         (
             'cimport foo\n',
-            't.py:1:9: Name foo imported but unused\n',
+            't.py:1:9: \'foo\' imported but unused\n',
         ),
         (
             'from foo cimport bar\n',
-            't.py:1:18: Name bar imported but unused\n',
+            't.py:1:18: \'bar\' imported but unused\n',
         ),
         (
             'from foo import bar, bar2\n',
-            't.py:1:17: Name bar imported but unused\n'
-            't.py:1:22: Name bar2 imported but unused\n',
+            't.py:1:17: \'bar\' imported but unused\n'
+            't.py:1:22: \'bar2\' imported but unused\n',
         ),
-    ]
+    ],
 )
-def test_imported_unused(capsys, src, expected):
+def test_imported_unused(capsys: Any, src: str, expected: str) -> None:
     ret = main(src, 't.py')
     out, _ = capsys.readouterr()
     assert out == expected
     assert ret == 1
 
+
 @pytest.mark.parametrize(
     'src',
     [
-            'cdef bint foo():\n'
-            '    raise NotImplemen',
-            'cdef bint foo():\n'
-            '    cdef int i\n'
-            '    for i in bar: pass\n',
-            'cdef bint foo(a):\n'
-            '   pass\n',
-            'cdef bint foo(int a):\n'
-            '   pass\n',
-            'cdef bint foo(int *a):\n'
-            '   pass\n',
-            'cdef bint* foo(int a):\n'
-            '   pass\n',
-            'cdef bint foo():\n'
-            '    cdef int i\n'
-            '    for i, j in bar: pass\n',
-            'cdef bint foo(object (*operation)(int64_t value, object right)):\n'
-            '   pass\n',
-            'import numpy as np\n',  # todo: should detect this
-            'class Foo: pass\n',
-            'cdef class Foo: pass\n',
-            'cdef bint foo(a):\n'
-            '    bar(<int>a)\n',
-            'cdef bint foo(a):\n'
-            '    bar(i for i in a)\n',
-            'cdef bint foo(a):\n'
-            '    bar(lambda x: x)\n',
-            'import int64_t\n'
-            'ctypedef fused foo:\n'
-            '    int64_t\n'
-            '    int32_t\n',
-            'import quox\n'
-            f'include "{INCLUDE_FILE}"\n'
-    ]
+        'cdef bint foo():\n'
+        '    raise NotImplemen',
+        'cdef bint foo():\n'
+        '    cdef int i\n'
+        '    for i in bar: pass\n',
+        'cdef bint foo(a):\n'
+        '   pass\n',
+        'cdef bint foo(int a):\n'
+        '   pass\n',
+        'cdef bint foo(int *a):\n'
+        '   pass\n',
+        'cdef bint* foo(int a):\n'
+        '   pass\n',
+        'cdef bint foo():\n'
+        '    cdef int i\n'
+        '    for i, j in bar: pass\n',
+        'cdef bint foo(object (*operation)(int64_t value, object right)):\n'
+        '   pass\n',
+        'import numpy as np\n',  # todo: should detect this
+        'class Foo: pass\n',
+        'cdef class Foo: pass\n',
+        'cdef bint foo(a):\n'
+        '    bar(<int>a)\n',
+        'cdef bint foo(a):\n'
+        '    bar(i for i in a)\n',
+        'cdef bint foo(a):\n'
+        '    bar(lambda x: x)\n',
+        'import int64_t\n'
+        'ctypedef fused foo:\n'
+        '    int64_t\n'
+        '    int32_t\n',
+        'import quox\n'
+        f'include "{INCLUDE_FILE}"\n',
+    ],
 )
-def test_noop(capsys, src):
+def test_noop(capsys: Any, src: str) -> None:
     ret = main(src, 't.py')
     out, _ = capsys.readouterr()
     assert out == ''
